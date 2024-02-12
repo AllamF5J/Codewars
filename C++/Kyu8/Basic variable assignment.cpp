9b0decf48a6dc74d48758bfffc353b2f804d02dc
Ahmed Allam <lamahmd122@gmail.com>
2024-02-13 01:55:10 +0200

    Basic variable assignment

---
+++ C++/Kyu8/Basic variable assignment.cpp
@@ -0,0 +1,10 @@
+/*
+|_______________________________________________________________________________
+| @Solver         : GziXnine.
+| @Link           : https://www.codewars.com/kata/50ee6b0bdeab583673000025
+| @description    : Concatenate strings "code" and "wa.rs" to form "codewa.rs".
+|_______________________________________________________________________________
+*/
+std::string a = "code";
+std::string b = "wa.rs";
+std::string name = a.append(b);
