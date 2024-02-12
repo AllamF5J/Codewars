a5e9b49a3bca4df7c323c7ddda64e254a9df3841
Ahmed Allam <lamahmd122@gmail.com>
2024-02-13 01:45:04 +0200

    All Star Code Challenge #18

---
+++ C++/Kyu8/All Star Code Challenge #18.cpp
@@ -0,0 +1,35 @@
+/*
+|_______________________________________________________________________________
+| @Solver         : GziXnine.
+| @Link           : https://www.codewars.com/kata/5865918c6b569962950002a1
+| @description    : Create a function that accepts a string and a single character, and returns an integer 
+|                   of the count of occurrences the 2nd argument is found in the first one.
+|                   If no occurrences can be found, a count of 0 should be returned.
+|
+|                   Examples:
+|                   - ("Hello", "o")  ==>  1
+|                   - ("Hello", "l")  ==>  2
+|                   - ("", "z")       ==>  0
+|
+|                   Function Call Examples:
+|                   - str_count("Hello", 'o'); // returns 1
+|                   - str_count("Hello", 'l'); // returns 2
+|                   - str_count("", 'z');      // returns 0
+|
+|                   Notes:
+|                   - The first argument can be an empty string.
+|                   - In languages with no distinct character data type, the second argument will be a string of length 1.
+|_______________________________________________________________________________
+*/
+#include <string>
+
+unsigned int strCount(const std::string& word, char letter) {
+  std::string newWord = word;
+  int count = 0;
+  for (int x : newWord) {
+    if (char(x) == letter) {
+      count++;
+    }
+  }
+  return count;
+}
