7dcdf8e9a68d1ac60f15bdc39b757bced6f22eef
Ahmed Allam <lamahmd122@gmail.com>
2024-02-13 16:53:54 +0200

    Hex to Decimal

---
+++ JavaScript/Kyu8/Hex to Decimal.js
@@ -0,0 +1,11 @@
+/*
+|_______________________________________________________________________________
+| @Solver         : GziXnine.
+| @Link           : https://www.codewars.com/kata/57a4d500e298a7952100035d
+| @description    : Complete the function which converts hex number (given as a string)
+|                   to a decimal number.
+|_______________________________________________________________________________
+*/
+function hexToDec(hexString){
+  return parseInt(hexString, 16);
+}
