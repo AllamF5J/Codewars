35e77d9d9c307678af48165e3450f43e4f29f073
Ahmed Allam <lamahmd122@gmail.com>
2024-02-13 04:12:49 +0200

    Return Negative

---
+++ C/kyu8/Return Negative.c
@@ -0,0 +1,11 @@
+/*
+|_______________________________________________________________________________
+| @Solver         : GziXnine.
+| @Link           : https://www.codewars.com/kata/55685cd7ad70877c23000102
+| @description    : Make a given number negative.
+|                   If the number is already negative, no change is required.
+|                   Note: Zero (0) is not checked for any specific sign.
+|                         Negative zeros make no mathematical sense.
+|_______________________________________________________________________________
+*/
+int makeNegative(int num) { return -abs(num); }
