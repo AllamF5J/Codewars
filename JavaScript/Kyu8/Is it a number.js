2aa3d3172ba4241604a947b922162debe1e93b15
Ahmed Allam <lamahmd122@gmail.com>
2024-02-13 16:54:56 +0200

    Is it a number

---
+++ JavaScript/Kyu8/Is it a number.js
@@ -0,0 +1,11 @@
+/*
+|_______________________________________________________________________________
+| @Solver         : User's implementation using parseFloat.
+| @Link           : https://www.codewars.com/kata/57126304cdbf63c6770012bd
+| @description    : Given a string s, this function returns true if it's a valid
+|                   single integer or floating number, and false otherwise.
+|_______________________________________________________________________________
+*/
+function isDigit(s) {
+  return s == parseFloat(s);
+}
