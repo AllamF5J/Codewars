6ee81b303141aa6e95757e15f026b6dd1c6d60d6
Ahmed Allam <lamahmd122@gmail.com>
2024-02-13 16:52:04 +0200

    Convert a string to an array

---
+++ JavaScript/Kyu8/Convert a string to an array.js
@@ -0,0 +1,10 @@
+/*
+|_______________________________________________________________________________
+| @Solver         : GziXnine.
+| @Link           : https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+| @description    : Split a string into an array of words.
+|_______________________________________________________________________________
+*/
+function stringToArray(string) {
+  return string.split(' ');
+}
