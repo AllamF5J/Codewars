5e1e3ce297508f2dc30b2aee9fa10454d7b1a570
Ahmed Allam <lamahmd122@gmail.com>
2024-02-13 17:03:45 +0200

    Merge two sorted arrays into one

---
+++ JavaScript/Kyu8/Merge two sorted arrays into one.js
@@ -0,0 +1,15 @@
+/*
+|_______________________________________________________________________________
+| @Solver         : User's implementation to merge and sort two arrays.
+| @Link           :https://www.codewars.com/kata/5899642f6e1b25935d000161
+| @description    : Given two sorted arrays, arr1 and arr2, the function merges
+|                   them into a single array, sorts it in ascending order, and
+|                   removes any duplicate elements.
+|_______________________________________________________________________________
+*/
+function mergeArrays(arr1, arr2) {
+  let newArr = arr1.concat(arr2);
+  let sortedArr = newArr.sort((a, b) => a - b);
+  let set = [...new Set(sortedArr)];
+  return set;
+}
