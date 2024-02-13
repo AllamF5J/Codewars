208eb636c7dd7df870d7bc68c3de97d32dd30bff
Ahmed Allam <lamahmd122@gmail.com>
2024-02-13 04:13:14 +0200

    Volume of a Cuboid

---
+++ C/kyu8/Volume of a Cuboid.c
@@ -0,0 +1,11 @@
+/*
+|_______________________________________________________________________________
+| @Solver         : GziXnine.
+| @Link           : https://www.codewars.com/kata/58261acb22be6e2ed800003a
+| @description    : Calculate the volume of a cuboid with given length, width,
+and height.
+|_______________________________________________________________________________
+*/
+double getVolumeOfCuboid(double length, double width, double height) {
+  return length * width * height;
+}
