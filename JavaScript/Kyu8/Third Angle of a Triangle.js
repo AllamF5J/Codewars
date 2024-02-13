2c15cc5b7110f48a689c08e21d678ced542e1377
Ahmed Allam <lamahmd122@gmail.com>
2024-02-13 17:07:12 +0200

    Third Angle of a Triangle

---
+++ JavaScript/Kyu8/Third Angle of a Triangle.js
@@ -0,0 +1,11 @@
+/*
+|_______________________________________________________________________________
+| @Solver         : GziXnine
+| @Link           : https://www.codewars.com/kata/5a023c426975981341000014/javascript
+| @description    : Given two interior angles 'a' and 'b' of a triangle, the
+|                   function calculates and returns the third angle.
+|____________________________________________________
+*/
+function otherAngle(a, b) {
+  return 180 - (a + b);
+}
