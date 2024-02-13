9981d4028b50335c3588c5055ee9ff4275f6f06e
Ahmed Allam <lamahmd122@gmail.com>
2024-02-13 16:51:05 +0200

    A Needle in the Haystack

---
+++ JavaScript/Kyu8/A Needle in the Haystack.js
@@ -0,0 +1,10 @@
+/*
+|_______________________________________________________________________________
+| @Solver         : GziXnine.
+| @Link           : https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/javascript
+| @description    : Find the needle in the haystack array and return its position.
+|_______________________________________________________________________________
+*/
+function findNeedle(haystack) {
+  return `found the needle at position ${haystack.indexOf('needle')}`;
+}
