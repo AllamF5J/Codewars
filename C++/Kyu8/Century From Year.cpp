f6f9e80db56f03f2a0b6f6ce0ae7b4428e44dd4f
Ahmed Allam <lamahmd122@gmail.com>
2024-02-15 21:48:38 +0200

    Century From Year

---
+++ C++/Kyu8/Century From Year.cpp
@@ -0,0 +1,23 @@
+/*
+|_______________________________________________________________________________
+| @Solver         : GziXnine
+| @Link           : https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097
+| @description    : Introduction
+|                   The first century spans from the year 1 up to and including
+|                   the year 100, the second century - from the year 101 up to
+|                   and including the year 200, etc.
+|                   Task
+|                   Given a year, return the century it is in.
+|                   Examples
+|                   1705 --> 18
+|                   1900 --> 19
+|                   1601 --> 17
+|                   2000 --> 20
+|                   2742 --> 28
+|_______________________________________________________________________________
+*/
+#include <iostream>
+
+int centuryFromYear(int year) {
+    return (year + 99) / 100;
+}
