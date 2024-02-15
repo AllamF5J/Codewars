0eb64d790d23f94819b09c333459543c4e646d3b
Ahmed Allam <lamahmd122@gmail.com>
2024-02-15 21:51:41 +0200

    Convert boolean values to strings Yes or No

---
+++ C++/Kyu8/Convert boolean values to strings Yes or No.cpp
@@ -0,0 +1,14 @@
+/*
+|_______________________________________________________________________________
+| @Solver         : GziXnine
+| @Link           : https://www.codewars.com/kata/53369039d7ab3ac506000467
+| @description    : Complete the method that takes a boolean value and return
+|                   a "Yes" string for true, or a "No" string for false.
+|_______________________________________________________________________________
+*/
+
+#include <string>
+
+std::string bool_to_word(bool value) {
+    return value ? "Yes" : "No";
+}
