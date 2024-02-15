09c475d3e019bdbc0b9c4e434452524db28ce0df
Ahmed Allam <lamahmd122@gmail.com>
2024-02-15 21:59:38 +0200

    Repare Syntax

--- C++/Kyu4/4 By 4 Skyscrapers.cpp
+++ C++/Kyu4/4 By 4 Skyscrapers.cpp
@@ -18,8 +18,10 @@
 
 using namespace std;
 
-bool test(int size, int** board, const int* clues, int row, int col) {
-    for (int i = 0; i < size; i++) {
+bool test(int size, int** board, const int* clues, int row, int col) 
+{
+    for (int i = 0; i < size; i++) 
+    {
         if (board[row][i] == board[row][col] && i != col) return false;
         if (board[i][col] == board[row][col] && i != row) return false;
     }
@@ -28,48 +30,60 @@ bool test(int size, int** board, const int* clues, int row, int col) {
     int visible;
     int max_height;
 
-    if ((clue = clues[col]) != 0) {
+    if ((clue = clues[col]) != 0) 
+    {
         visible = max_height = 0;
         complete = true;
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) 
+        {
             if (board[i][col] == 0) complete = false;
-            else if (board[i][col] > max_height) {
+            else if (board[i][col] > max_height) 
+            {
                 visible++;
                 if ((max_height = board[i][col]) == size) break;
             }
         }
         if (complete && visible != clue) return false;
     }
-    if ((clue = clues[row + size]) != 0) {
+    if ((clue = clues[row + size]) != 0) 
+    {
         visible = max_height = 0;
         complete = true;
-        for (int i = size - 1; i >= 0; i--) {
+        for (int i = size - 1; i >= 0; i--) 
+        {
             if (board[row][i] == 0) complete = false;
-            else if (board[row][i] > max_height) {
+            else if (board[row][i] > max_height) 
+            {
                 visible++;
                 if ((max_height = board[row][i]) == size) break;
             }
         }
         if (complete && visible != clue) return false;
     }
-    if ((clue = clues[3 * size - col - 1]) != 0) {
+    if ((clue = clues[3 * size - col - 1]) != 0) 
+    {
         visible = max_height = 0;
         complete = true;
-        for (int i = size - 1; i >= 0; i--) {
+        for (int i = size - 1; i >= 0; i--) 
+        {
             if (board[i][col] == 0) complete = false;
-            else if (board[i][col] > max_height) {
+            else if (board[i][col] > max_height) 
+            {
                 visible++;
                 if ((max_height = board[i][col]) == size) break;
             }
         }
         if (complete && visible != clue) return false;
     }
-    if ((clue = clues[4 * size - row - 1]) != 0) {
+    if ((clue = clues[4 * size - row - 1]) != 0) 
+    {
         visible = max_height = 0;
         complete = true;
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) 
+        {
             if (board[row][i] == 0) complete = false;
-            else if (board[row][i] > max_height) {
+            else if (board[row][i] > max_height) 
+            {
                 visible++;
                 if ((max_height = board[row][i]) == size) break;
             }
@@ -79,21 +93,27 @@ bool test(int size, int** board, const int* clues, int row, int col) {
     return true;
 }
 
-int** SolvePuzzle (int *clues) {
+int** SolvePuzzle (int *clues) 
+{
     int size = 4;
     int** board = new int*[size];
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) 
+    {
         board[i] = new int[size];
         for (int j = 0; j < size; j++)
             board[i][j] = 0;
     }
-    for (int row = 0; row < size; row++) {
-        for (int col = 0; col < size; col++) {
+    for (int row = 0; row < size; row++) 
+    {
+        for (int col = 0; col < size; col++) 
+        {
             while (++board[row][col] <= size && !test(size, board, clues, row, col));
-            if (board[row][col] > size) {
+            if (board[row][col] > size) 
+            {
                 board[row][col] = 0;
                 col -= 2;
-                if (col < 0) {
+                if (col < 0) 
+                {
                     col += size;
                     row--;
                 }
