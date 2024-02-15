825845f51015eb1c58c44ae9c2692cf955969f24
Ahmed Allam <lamahmd122@gmail.com>
2024-02-15 21:55:55 +0200

    Counting sheep

---
+++ C++/Kyu8/Counting sheep.cpp
@@ -0,0 +1,33 @@
+/*
+|_______________________________________________________________________________
+| @Solver         : GziXnine
+| @Link           : https://www.codewars.com/kata/54edbc7200b811e956000556
+| @description    : Consider an array/list of sheep where some sheep may be
+|                   missing from their place. We need a function that counts
+|                   the number of sheep present in the array (true means present).
+|
+|                   For example,
+|                   [true,  true,  true,  false,
+|                    true,  true,  true,  true ,
+|                    true,  false, true,  false,
+|                    true,  false, false, true ,
+|                    true,  true,  true,  true ,
+|                    false, false, true,  true]
+|
+|                   The correct answer would be 17.
+|
+|                   Hint: Don't forget to check for bad values like null/undefined
+|_______________________________________________________________________________
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int count_sheep(vector<bool> arr) 
+{
+    int x = 0;
+    for(int y : arr)
+        if(y == 1) x +=1;
+    return x;
+}
