1a4d2e8b5f0a1ba7d5580c476ed137fe040bb3f2
Ahmed Allam <lamahmd122@gmail.com>
2024-02-15 21:56:20 +0200

    Convert a Number to a String!

---
+++ C++/Kyu8/Convert a Number to a String!.cpp
@@ -0,0 +1,19 @@
+/*
+|_______________________________________________________________________________
+| @Solver         : GziXnine
+| @Link           : https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+| @description    : We need a function that can transform a number (integer)
+|                   into a string.
+|                   What ways of achieving this do you know?
+|                   Examples (input --> output):
+|                   123  --> "123"
+|                   999  --> "999"
+|                   -100 --> "-100"
+|_______________________________________________________________________________
+*/
+#include <iostream>
+#include <string>
+
+std::string number_to_string(int num) {
+    return std::to_string(num);
+}
