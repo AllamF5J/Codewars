e2b0e8dbb8259cd737e73bfb9bb0ea7c4ee681d4
Ahmed Allam <lamahmd122@gmail.com>
2024-02-16 21:59:53 +0200

    DNA to RNA Conversion

---
+++ C++/Kyu8/DNA to RNA Conversion.cpp
@@ -0,0 +1,33 @@
+/*
+|_______________________________________________________________________________
+| @Solver         : GziXnine
+| @Link           : https://www.codewars.com/kata/5556282156230d0e5e000089
+| @description    : Deoxyribonucleic acid, DNA is the primary information storage
+|                   molecule in biological systems. It is composed of four nucleic
+|                   acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and
+|                   Thymine ('T').
+|
+|                   Ribonucleic acid, RNA, is the primary messenger molecule in cells.
+|                   RNA differs slightly from DNA its chemical structure and contains
+|                   no Thymine. In RNA Thymine is replaced by another nucleic acid
+|                   Uracil ('U').
+|
+|                   Create a function which translates a given DNA string into RNA.
+|
+|                   For example:
+|                   "GCAT"  =>  "GCAU"
+|                   The input string can be of arbitrary length - in particular,
+|                   it may be empty. All input is guaranteed to be valid, i.e.
+|                   each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+|_______________________________________________________________________________
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+std::string DNAtoRNA(std::string dna) {
+    for (char &x : dna)
+        x = x == 'T' ? 'U' : x;
+    return dna;
+}
