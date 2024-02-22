699ac4f8c6bae9252948c346d5978f27830c7968
Ahmed Allam <lamahmd122@gmail.com>
2024-02-22 23:03:26 +0200

    Grasshopper - Personalized Message

---
+++ C++/Kyu8/Grasshopper - Personalized Message.cpp
@@ -0,0 +1,20 @@
+/*
+|_______________________________________________________________________________
+| @Solver         : GziXnine
+| @Link           : https://www.codewars.com/kata/5772da22b89313a4d50012f7
+| @description    : Create a function that gives a personalized greeting.
+|                   This function takes two parameters: name and owner.
+|
+|                   Use conditionals to return the proper message:
+|
+|                   case      return
+|                   name equals owner    'Hello boss'
+|                   otherwise 'Hello guest'
+|_______________________________________________________________________________
+*/
+#include <string>
+
+std::string greet(const std::string &name, const std::string &owner)
+{
+  return name == owner ? "Hello boss" : "Hello guest";
+}
