49ed9beaa019cd01d305c087c13981a817e951b7
Ahmed Allam <lamahmd122@gmail.com>
2024-02-25 05:02:11 +0200

    L1: Set Alarm

---
+++ C++/Kyu8/L1 Set Alarm.cpp
@@ -0,0 +1,25 @@
+/*
+|_______________________________________________________________________________
+| @Solver         : GziXnine
+| @Link           : https://www.codewars.com/kata/568dcc3c7f12767a62000038
+| @description    : Write a function named setAlarm which receives two parameters.
+|                   The first parameter, employed, is true whenever you are employed
+|                   and the second parameter, vacation is true whenever you are on
+|                   vacation.
+|
+|                   The function should return true if you are employed and not on
+|                   vacation (because these are the circumstances under which you
+|                   need to set an alarm). It should return false otherwise.
+|
+|                   Examples:
+|                   - employed | vacation  => false
+|                   - true     | true      => false
+|                   - true     | false     => true
+|                   - false    | true      => false
+|                   - false    | false     => false
+|_______________________________________________________________________________
+*/
+bool set_alarm(const bool &employed, const bool &vacation)
+{
+  return employed == 1 && vacation == 0;
+}
