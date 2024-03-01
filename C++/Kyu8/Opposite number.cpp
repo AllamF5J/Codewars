689919fe130d62e6b8270552d7cd232cdade39ee
Ahmed Allam <lamahmd122@gmail.com>
2024-03-01 18:34:09 +0200

    Opposite number

---
+++ C++/Kyu8/Opposite number.cpp
@@ -0,0 +1,18 @@
+/*
+|_______________________________________________________________________________
+| @Solver         : GziXnine
+| @Link           : https://www.codewars.com/kata/56dec885c54a926dcd001095
+| @description    : Very simple, given a number (integer / decimal / both
+|                   depending on the language), find its opposite (additive
+|                   inverse).
+|
+|                   Examples:
+|                   1: -1
+|                   14: -14
+|                   -34: 34
+|_______________________________________________________________________________
+*/
+int opposite(int number)
+{
+  return -number;
+}
