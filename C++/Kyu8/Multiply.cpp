115ab6da995540cb6b9b46a6b3c0be5489a6f601
Ahmed Allam <lamahmd122@gmail.com>
2024-03-01 18:31:25 +0200

    Multiply

---
+++ C++/Kyu8/Multiply.cpp
@@ -0,0 +1,11 @@
+/*
+|_______________________________________________________________________________
+| @Solver         : GziXnine
+| @Link           : https://www.codewars.com/kata/50654ddff44f800200000004
+| @description    : This code does not execute properly. Try to figure out why.
+|_______________________________________________________________________________
+*/
+int multiply(int a, int b)
+{
+  return a * b;
+}
