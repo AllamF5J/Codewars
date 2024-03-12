69daea545afcd59abe6a4665a3b0bb01894faf96
Ahmed Allam <lamahmd122@gmail.com>
2024-03-12 15:09:48 +0200

    Student's Final Grade

---
+++ C++/Kyu8/Student's Final Grade.cpp
@@ -0,0 +1,40 @@
+/*
+|_______________________________________________________________________________
+| @Solver         : GziXnine
+| @Link           : https://www.codewars.com/kata/5ad0d8356165e63c140014d4
+| @description    : Create a function finalGrade, which calculates the final
+|                   grade of a student depending on two parameters: a grade for
+|                   the exam and a number of completed projects.
+|
+|                   This function should take two arguments:
+|                   - exam: grade for the exam (from 0 to 100);
+|                   - projects: number of completed projects (from 0 and above);
+|
+|                   This function should return a number (final grade). There
+|                   are four types of final grades:
+|                   - 100, if a grade for the exam is more than 90 or if a
+|                     number of completed projects is more than 10.
+|                   - 90, if a grade for the exam is more than 75 and if a
+|                     number of completed projects is minimum 5.
+|                   - 75, if a grade for the exam is more than 50 and if a
+|                     number of completed projects is minimum 2.
+|                   - 0, in other cases.
+|
+|                   Examples (Inputs --> Output):
+|                   - 100, 12 --> 100
+|                   - 99, 0 --> 100
+|                   - 10, 15 --> 100
+|                   - 85, 5 --> 90
+|                   - 55, 3 --> 75
+|                   - 55, 0 --> 0
+|                   - 20, 2 --> 0
+|
+|                   *Use Comparison and Logical Operators.
+|_______________________________________________________________________________
+*/
+int final_grade(int exam, int projects)
+{
+  return exam > 90 || projects > 10 ? 100 : exam > 75 && projects >= 5 ? 90
+                                        : exam > 50 && projects >= 2   ? 75
+                                                                       : 0;
+}
