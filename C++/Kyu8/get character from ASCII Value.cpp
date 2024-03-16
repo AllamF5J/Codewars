5232a5830d3a66d068221ba69435395b60d24fc3
Ahmed Allam <lamahmd122@gmail.com>
2024-03-16 23:44:23 +0200

    get character from ASCII Value

---
+++ C++/Kyu8/get character from ASCII Value.cpp
@@ -0,0 +1,15 @@
+/*
+|_______________________________________________________________________________
+| @Solver         : GziXnine
+| @Link           : https://www.codewars.com/kata/55ad04714f0b468e8200001c
+| @description    : Write a function which takes a number and returns the corresponding ASCII char for that value.
+|                   Example:
+|                   65 --> 'A'
+|                   97 --> 'a'
+|                   48 --> '0'
+|_______________________________________________________________________________
+*/
+char get_char(int i)
+{
+  return char(i);
+}
