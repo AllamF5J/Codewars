c8934bb39f4f6a230be2772c49a06baa9aedd1a0
Ahmed Allam <lamahmd122@gmail.com>
2024-03-16 23:40:00 +0200

    You Can't Code Under Pressure #1

---
+++ C++/Kyu8/You Can't Code Under Pressure #1.cpp
@@ -0,0 +1,14 @@
+/*
+|_______________________________________________________________________________
+| @Solver         : GziXnine
+| @Link           : https://www.codewars.com/kata/53ee5429ba190077850011d4
+| @description    : Code as fast as you can! You need to double the integer and
+|                   return it.
+|_______________________________________________________________________________
+*/
+#include <cstdint>
+
+int32_t double_integer( int32_t n )
+{
+  return n * 2;
+}
